d7213df3ed3522169513bf0f2c3d7ed2d5bb6681
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-06-14 12:03:53 -0400

    added final solution

---
+++ script.js
@@ -0,0 +1,40 @@
+    // 1 - Select the section with an id of container without using querySelector.
+    q1 = document.getElementById("container");
+
+    // 2 - Select the section with an id of container using querySelector.
+    document.querySelector("#container");
+
+    // 3 - Select all of the list items with a class of "second".
+    document.querySelector(".second");
+
+    // 4 - Select a list item with a class of third, but only the list item inside of the ol tag.
+    document.querySelector("ol .second");
+
+    // 5 - Give the section with an id of container the text "Hello!".
+    //q1.innerText = "Hello";
+
+    // 6 - Add the class main to the div with a class of footer.
+    q6 = document.querySelector(".footer");
+    q6.classList.add("main");
+    
+    // 7 - Remove the class main on the div with a class of footer.
+    q6.classList.remove("main");
+
+    // 8 - Create a new li element.
+    q8 = document.createElement("li");
+
+    // 9 - Give the li the text "four".
+    q8.innerText = "four";
+
+    // 10 - Append the li to the ul element.
+    document.querySelector("ul").append(q8);
+
+    // 11 - Loop over all of the lis inside the ol tag and give them a background color of "green".
+    q11 = document.querySelectorAll("ol li");
+    for (let i = 0; i < q11.length; i++) {
+        q11[i].style.backgroundColor = "green";
+    }
+
+    // 12 - Remove the div with a class of footer.
+    q12 = document.querySelector(".footer");
+    q12.remove();
